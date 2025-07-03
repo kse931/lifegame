72356944637461be44b6776d85ede9c1a377c55d
kse391 <kse04199@gmail.com>
2025-07-03 11:40:31 +0700

    deleted useless

--- src/Game.java
+++
@@ -1,9 +0,0 @@
-
-public class Game {
-
-    public static void main(String[] args) {
-        Window window = new Window();
-        javax.swing.SwingUtilities.invokeLater(window);
-        //ne bipki
-    }
-}
